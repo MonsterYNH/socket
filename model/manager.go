c0ee5616d47f7394e006668462b469823f6bdb73
monster <ynhmonster@163.com>
2019-05-29 21:32:06 +0800

    update

--- model/manager.go
+++ model/manager.go
@@ -7,26 +7,26 @@ import (
 )
 
 type Manager struct {
-	ConnMap map[string]*Client // 维护的连接池
-	register chan *Client // 注册一个client
-	unRegister chan *Client // 注销一个client
-	broad chan []byte // 广播消息
-	one chan clientMessage // 单独发送消息
-	eventPool map[string]func(client *Client)error // 事件管理池
+	ConnMap    map[string]*Client                    // 维护的连接池
+	register   chan *Client                          // 注册一个client
+	unRegister chan *Client                          // 注销一个client
+	broad      chan []byte                           // 广播消息
+	one        chan clientMessage                    // 单独发送消息
+	eventPool  map[string]func(client *Client) error // 事件管理池
 }
 
 type clientMessage struct {
-	connID string
+	connID  string
 	message []byte
 }
 
 func CreateManager() *Manager {
-	manager :=  &Manager {
-		ConnMap: make(map[string]*Client),
-		register: make(chan *Client),
+	manager := &Manager{
+		ConnMap:    make(map[string]*Client),
+		register:   make(chan *Client),
 		unRegister: make(chan *Client),
-		broad: make(chan []byte),
-		eventPool: make(map[string]func(client *Client)error),
+		broad:      make(chan []byte),
+		eventPool:  make(map[string]func(client *Client) error),
 	}
 	manager.RegistEvent("OnConnect", OnConnect)
 	manager.RegistEvent("OnClose", OnClose)
@@ -37,17 +37,17 @@ func CreateManager() *Manager {
 func (manager *Manager) run() {
 	for {
 		select {
-		case client := <- manager.register:
+		case client := <-manager.register:
 			manager.ConnMap[client.ConnID] = client
-		case client := <- manager.unRegister:
+		case client := <-manager.unRegister:
 			if client, exist := manager.ConnMap[client.ConnID]; exist {
 				delete(manager.ConnMap, client.ConnID)
 			}
-		case message := <- manager.broad:
+		case message := <-manager.broad:
 			for _, client := range manager.ConnMap {
 				client.SendMessage(message)
 			}
-		case data := <- manager.one:
+		case data := <-manager.one:
 			_, exist := manager.ConnMap[data.connID]
 			fmt.Println("status: ", exist)
 			if exist {
@@ -90,12 +90,12 @@ func (manager *Manager) Broad(data []byte) {
 
 func (manager *Manager) BoardOne(connID string, message []byte) {
 	manager.one <- clientMessage{
-		connID: connID,
+		connID:  connID,
 		message: message,
 	}
 }
 
-func (manager *Manager) RegistEvent(eventName string, callback func(client *Client)error) {
+func (manager *Manager) RegistEvent(eventName string, callback func(client *Client) error) {
 	manager.eventPool[eventName] = callback
 }
 
